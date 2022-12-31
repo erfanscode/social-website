ef123532219d424500548d90080af16b7e26e0d6
erfanscode <96702228+erfanscode@users.noreply.github.com>
2022-12-31 21:47:12 +0330

    add password change user

--- account/urls.py
+++ account/urls.py
@@ -3,9 +3,14 @@ from django.contrib.auth import views as auth_views
 from .views import dashboard
 
 urlpatterns = [
-    # path('login/', user_login, name='login'),
-
+    # Authentication
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # Password Change
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # Local
     path("", dashboard, name='dashboard'),
 ]
