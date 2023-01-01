a1b559faf1e73db407cd06dbb7d785aa148c5624
erfanscode <96702228+erfanscode@users.noreply.github.com>
2023-01-02 00:22:38 +0330

    form configuration for edit profile

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserRegistrationForm(forms.ModelForm):
     username = forms.CharField(max_length=100)
@@ -18,3 +19,15 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Password dont match')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['date_of_birth', 'photo']
