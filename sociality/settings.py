ab3b207ae4f8b81cdad39a9f8b3fc746a6c26bfa
erfanscode <96702228+erfanscode@users.noreply.github.com>
2023-01-09 15:35:57 +0330

    add authentication.py and user can login with email/username

--- sociality/settings.py
+++ sociality/settings.py
@@ -108,6 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "account.authentication.EmailAuthBackend",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
