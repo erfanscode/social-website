14f9e7e6e4c02ebaaec5fdf0828fab48e2546043
erfanscode <96702228+erfanscode@users.noreply.github.com>
2023-01-25 01:11:54 +0330

    Add custom pipeline step for saving user profile in social auth pipeline

--- sociality/settings.py
+++ sociality/settings.py
@@ -122,6 +122,19 @@ AUTHENTICATION_BACKENDS = [
     "social_core.backends.google.GoogleOAuth2",
 ]
 
+# Add pipeline:
+SOCIAL_AUTH_PIPELINE = [
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
